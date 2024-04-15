938c9ef51a173ff721cb7416e6a11d5646bb2cf0
davidluupli <davidoluyale@gmail.com>
2024-04-15 13:39:58 +0100

    flask cor

--- main.py
+++ main.py
@@ -131,11 +131,9 @@ async def handler(websocket, path):
 # start_server = websockets.serve(handler, "", 5000)
 # start_server = websockets.serve(handler, None, 0)
 
-start_server = websockets.serve(handler, port=5000)
+start_server = websockets.serve(handler, "0.0.0.0", port=5000)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
 # if __name__ == "__main__":
 #     app.run()
- 
- 
