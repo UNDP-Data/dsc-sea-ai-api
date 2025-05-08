d2b864381d122a1b057a0d18f2a0f72d37a1f7fc
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-05-08 14:23:13 +0200

    Refactor(main): simplify the llm endpoint
    Combine reduce duplication for full and not-full response types.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 Entry point to the API.
 """
 
-import json
 from collections import OrderedDict
 from typing import Annotated
 
@@ -36,68 +35,33 @@ async def get_kg_data(q: Annotated[list[str], Query()]):
 
 @app.post(path="/llm")
 async def send_prompt_llm(message: Message):
-    try:
-        user_query = message.content
-        response = {}
+    user_query = message.content
+    response = {}
 
-        if message.full:
+    # user is requering ... get all relevant answers
+    entities_dict = processing.get_knowledge_graph(user_query)
+    query_idea_list = processing.generate_query_ideas(user_query)
+    entities_array = list(entities_dict["entities"]) if entities_dict else []
+    if message.full:
+        excerpts_dict = processing.process_queries(user_query)
+        excerpts_dict_synthesis = processing.remove_thumbnails(excerpts_dict)
+        answer = processing.get_answer(user_query, excerpts_dict_synthesis)
+        kg_content = None
+    else:
+        excerpts_dict, answer = {}, "Processing final answer... "
+        kg_content = processing.find_kg(entities_array)
 
-            # user is requering ... get all relevant answers
-            entities_dict = processing.get_knowledge_graph(user_query)
-            query_idea_list = processing.generate_query_ideas(user_query)
-            excerpts_dict = processing.process_queries(user_query)
-            excerpts_dict_synthesis = processing.remove_thumbnails(excerpts_dict)
-            answer = processing.get_answer(user_query, excerpts_dict_synthesis)
+    response = OrderedDict(
+        [
+            ("answer", answer),
+            ("user_query", user_query),
+            ("entities", entities_array),
+            ("query_ideas", query_idea_list if query_idea_list else []),
+            ("excerpts_dict", excerpts_dict),
+            ("indicators_dict", {}),
+            ("kg_data", kg_content),
+        ]
+    )
 
-            response = OrderedDict(
-                [
-                    ("answer", answer),
-                    ("user_query", user_query),
-                    (
-                        "entities",
-                        (list(entities_dict["entities"]) if entities_dict else []),
-                    ),
-                    ("query_ideas", query_idea_list if query_idea_list else []),
-                    ("excerpts_dict", excerpts_dict),
-                    ("indicators_dict", {}),
-                ]
-            )
-
-            # Convert the response to a JSON string and then back to a dictionary to preserve order
-            response_json = json.dumps(response, indent=4)
-
-            # Return the response
-            return response_json
-        else:
-
-            entities_dict = processing.get_knowledge_graph(user_query)
-            query_idea_list = processing.generate_query_ideas(user_query)
-
-            # Get results from completed futures
-            entities_array = list(entities_dict["entities"]) if entities_dict else []
-
-            kg_content = processing.find_kg(entities_array)
-            response = {
-                "answer": "Processing final answer... ",
-                "user_query": user_query,
-                "entities": entities_array,
-                "query_ideas": query_idea_list if query_idea_list else [],
-                "excerpts_dict": {},
-                "indicators_dict": {},
-                "kg_data": kg_content,
-            }
-
-            # Return the response
-            return response
-
-    except Exception as e:
-        print(e)
-        # Return error response
-        return {
-            "status": "failed",
-            "message": "an error occurred",
-            "session_id": None,
-            "answers": [],
-            "entities": [],
-            "prompts": [],
-        }
+    # Return the response
+    return response
