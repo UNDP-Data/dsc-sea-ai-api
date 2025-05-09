898685d71a3bc1961aa3f0ebffae0e755738c3c9
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-05-09 10:54:47 +0200

    Perf(main): add a lifespan event manager
    This allows to load the database client once and reuse for all subsequent responses.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 Entry point to the API.
 """
 
+from contextlib import asynccontextmanager
 from typing import Annotated
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 
 from src import database, processing
 from src.entities import AssistantMessage, HumanMessage, KnowledgeGraph
@@ -13,7 +14,28 @@ from src.entities import AssistantMessage, HumanMessage, KnowledgeGraph
 load_dotenv()
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    """
+    Define the logic that should be executed before the application start up.
+
+    See the details in [the documentation](https://fastapi.tiangolo.com/advanced/events/).
+
+    Parameters
+    ----------
+    _ : FastAPI
+        Application object, not directly used but required by signature.
+
+    Yields
+    ------
+    states : dict
+        Dictionary of arbitrary state variables.
+    """
+    states = {"client": database.Client.from_model()}
+    yield states
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get(
@@ -31,10 +53,10 @@ async def get_kg_data(q: Annotated[list[str], Query()]):
     response_model=AssistantMessage,
     response_model_by_alias=False,
 )
-async def send_prompt_llm(message: HumanMessage):
+async def send_prompt_llm(request: Request, message: HumanMessage):
     user_query = message.content
     response = {}
-    client = database.Client.from_model()
+    client: database.Client = request.state.client
     # user is requering ... get all relevant answers
     entities_dict = processing.get_knowledge_graph(user_query)
     query_idea_list = processing.generate_query_ideas(user_query) or None
