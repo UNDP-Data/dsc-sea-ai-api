c02f78004317e2fbaa313b67f38eeae74cfaadec
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-05-13 21:49:02 +0200

    Feat(genai): support structured outputs in `generate_response`

--- src/genai.py
+++ src/genai.py
@@ -5,13 +5,14 @@ Functions for interacting with GenAI models via Azure OpenAI.
 import os
 
 from openai import AzureOpenAI
+from pydantic import BaseModel
 
 __all__ = ["get_client", "generate_response", "embed_text"]
 
 
 def get_client():
     client = AzureOpenAI(
-        api_version="2024-07-01-preview",
+        api_version="2024-12-01-preview",
         azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
         api_key=os.environ["AZURE_OPENAI_KEY"],
     )
@@ -22,19 +23,21 @@ def generate_response(
     prompt: str,
     system_message: str = "You are a helpful assistant.",
     **kwargs,
-) -> str:
+) -> str | BaseModel:
     # use the defaults if no kwargs are provided
     params = {"temperature": 0} | kwargs
     client = get_client()
-    response = client.chat.completions.create(
+    response = client.beta.chat.completions.parse(
         model=os.environ["CHAT_MODEL"],
         **params,
         messages=[
             {"role": "system", "content": system_message},
             {"role": "user", "content": prompt},
         ],
+        timeout=10,
     )
-    return response.choices[0].message.content
+    message = response.choices[0].message
+    return message.parsed if "response_format" in params else message.content
 
 
 def embed_text(text: str) -> list[float]:
