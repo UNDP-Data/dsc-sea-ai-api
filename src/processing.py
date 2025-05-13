d1662705775cafdbce7edde8a25b982ba3d33a02
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-05-13 21:26:05 +0200

    Database support (#84)
    * Build: add `lancedb` for a vector database support
    
    * Chore(storage)!: use account key to connect to Blob Storage
    
    * Refactor(main): only use required functions in `ask_model`
    
    Remove `get_knowledge_graph` from processing which adds overhead but whose outputs are not used.
    
    * Refactor(entities)!: rename `query_ideas` to `ideas` for brevity
    
    * Refactor(main)!: remove `full` parameter from `ask_model` to simplify
    
    * Feat(database)!: introduce LanceDB to simplify search and data schema
    
    * Refactor(src): remove unused `storage` module
    
    * Build: remove unused dependencies
    
    Retrieval and search operations are now handled by LanceDB.

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,8 @@
 import ast
-import copy
 import re
 
-from . import genai, storage
-from .entities import Subgraph
+from . import genai
+from .entities import Document
 
 
 # Extract entities for the query and return the extract entities as an array
@@ -18,37 +17,11 @@ def extract_entities(user_query: str) -> list[str]:
  
     """
     entity_list = genai.generate_response(prompt)
-    return entity_list
-
-
-## module to get information on the entities from user query using the KG
-def get_knowledge_graph(user_query: str) -> dict:
-
-    # generate list of entities based on user query
-    entity_list = extract_entities(user_query)
-    entity_list = ast.literal_eval(entity_list)
-    prompt_summarise_entites = f"""
-    Summarize all relations between all the entities : {entity_list}
-    """
-    summarise_entities = genai.generate_response(prompt_summarise_entites)
-    return {"relations": summarise_entities, "entities": entity_list}
-
-
-# Function to calculate Jaccard similarity between two texts
-def jaccard_similarity(text1: str, text2: str) -> float:
-    # Tokenize texts
-    tokens1 = set(text1.lower().split())
-    tokens2 = set(text2.lower().split())
-
-    # Calculate Jaccard similarity
-    intersection = len(tokens1.intersection(tokens2))
-    union = len(tokens1.union(tokens2))
-
-    return intersection / union if union > 0 else 0
+    return ast.literal_eval(entity_list)
 
 
 # get answer
-def get_answer(user_question: str, relevant_docs: dict[str, dict]) -> str:
+def get_answer(user_question: str, documents: list[Document]) -> str:
 
     formattings_html = f""" 
         Ignore previous
@@ -68,7 +41,7 @@ def get_answer(user_question: str, relevant_docs: dict[str, dict]) -> str:
     formattings = f""" 
         You can use relevant information in the docs to answer also: 
 
-        DOCS: {relevant_docs}
+        DOCS: {documents}
         
        """
     system_message = f"""You are a helpful assistant and a professional researcher with many years of experience in answering questions. Give answer to the user's inquiry. {formattings_html}"""
@@ -98,19 +71,6 @@ def get_answer(user_question: str, relevant_docs: dict[str, dict]) -> str:
     return cleaned_text
 
 
-def remove_thumbnails(data: dict[str, dict]) -> dict[str, dict]:
-    data_no_thumbnails = copy.deepcopy(data)  # Make a deep copy of the data
-    for doc_id, doc_info in data_no_thumbnails.items():
-        if "document_thumbnail" in doc_info:
-            del doc_info["document_thumbnail"]
-
-    for doc_id, doc_info in data_no_thumbnails.items():
-        if "content" in doc_info:
-            del doc_info["content"]
-
-    return data_no_thumbnails
-
-
 ## module to generate query ideas
 def generate_query_ideas(user_query: str) -> list[str]:  # lower priority
 
@@ -129,99 +89,3 @@ def generate_query_ideas(user_query: str) -> list[str]:  # lower priority
     response = genai.generate_response(prompt)
     query_ideas = response.split(" | ")
     return query_ideas
-
-
-# Function to calculate the similarity score between two strings
-def similarity_score_kg(word1: str, word2: str) -> float:
-    # Convert strings to lowercase for case-insensitive comparison
-    word1_lower = word1.lower()
-    word2_lower = word2[:-5].lower()
-
-    # Split strings into individual words
-    words1 = word1_lower.split()
-    words2 = word2_lower.split()
-
-    # Calculate the number of overlapping words
-    common_words = set(words1) & set(words2)
-
-    # Calculate similarity score as percentage of overlapping words
-    similarity = len(common_words) / max(len(words1), len(words2)) * 100
-
-    return similarity
-
-
-def find_kg(keywords: list[str]) -> list[Subgraph]:
-    data_dir = "KG"
-    max_score = 0
-    most_similar_file = None
-
-    # Extract the first keyword from the list
-    first_keyword = keywords[0] if keywords else None
-
-    # Calculate the similarity score with the first keyword
-    client = storage.get_blob_client()
-    for filename in client.find(f"{storage.CONTAINER_NAME}/{data_dir}"):
-        if filename.endswith(".json"):
-            filename = filename.split("/")[-1]
-            score = similarity_score_kg(first_keyword, filename)
-            if score > max_score:
-                max_score = score
-                most_similar_file = filename
-                # Break the loop after finding the first matching file
-                break
-
-    if most_similar_file is None:
-        print("No matching file found.")
-        return None
-
-    initial_root = most_similar_file[:-5]
-    initial_kg = {}
-    try:
-        initial_kg = storage.load_json(f"{data_dir}/{initial_root}.json")
-    except Exception as e:
-        print(f"Error loading initial root file: {e}")
-        return None
-
-    # Load the content of the most similar file
-    try:
-        content = storage.load_json(f"{data_dir}/{most_similar_file}")
-    except Exception as e:
-        print(f"Error loading most similar file: {e}")
-        return None
-
-    # Ensure content structure is correct
-    if (
-        "knowledge graph" not in content
-        or "relations" not in content["knowledge graph"]
-    ):
-        print("Invalid JSON structure.")
-        return None
-
-    found_files = [initial_kg]
-
-    # Iterate over each relation in the content
-    for relation, objects in content["knowledge graph"]["relations"].items():
-        for item in objects:
-            object_name = item.get("Object")
-
-            # Construct the paths for both original and lowercase filenames
-            json_file_original = f"{data_dir}/{object_name}.json"
-            json_file_lowercase = f"{data_dir}/{object_name.lower()}.json"
-
-            if storage.file_exists(json_file_original) or storage.file_exists(
-                json_file_lowercase
-            ):
-                json_file = (
-                    json_file_original
-                    if storage.file_exists(json_file_original)
-                    else json_file_lowercase
-                )
-
-                try:
-                    file_content = storage.load_json(json_file)
-                    found_files.append(file_content)
-                except Exception as e:
-                    print(f"Error loading file {json_file}: {e}")
-                    continue
-
-    return list(map(Subgraph.from_kg, found_files))
